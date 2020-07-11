42d8acafdbc54f2945f23007abc6cb55cffe7e63
Mariusz Jarosz <m.jarosz82@gmail.com>
2020-07-11 14:31:08 +0200

    created main block in cript.js file

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
-let moreFirstArticle = document.querySelector(".js-firstArticle");
-let moreSecondArticle = document.querySelector(".js-secondArticle");
-let readMoreFirstButton = document.querySelector(".js-firstButton");
-let readMoreSecondButton = document.querySelector(".js-secondButton");
+{
+    let moreFirstArticle = document.querySelector(".js-firstArticle");
+    let moreSecondArticle = document.querySelector(".js-secondArticle");
+    let readMoreFirstButton = document.querySelector(".js-firstButton");
+    let readMoreSecondButton = document.querySelector(".js-secondButton");
 
-readMoreFirstButton.addEventListener("click", () => {
-    if(readMoreFirstButton.innerText === "Pokaż więcej") {
-        moreFirstArticle.classList.toggle("visible");
-        readMoreFirstButton.innerText = "Pokaż mniej";
-    } else {
-        moreFirstArticle.classList.toggle("visible");
-        readMoreFirstButton.innerText = "Pokaż więcej";
-    }
-})
+    readMoreFirstButton.addEventListener("click", () => {
+        if (readMoreFirstButton.innerText === "Pokaż więcej") {
+            moreFirstArticle.classList.toggle("visible");
+            readMoreFirstButton.innerText = "Pokaż mniej";
+        } else {
+            moreFirstArticle.classList.toggle("visible");
+            readMoreFirstButton.innerText = "Pokaż więcej";
+        }
+    })
 
-readMoreSecondButton.addEventListener("click", () => {
-    if(readMoreSecondButton.innerText === "Pokaż więcej") {
-        moreSecondArticle.classList.toggle("visible");
-        readMoreSecondButton.innerText = "Pokaż mniej";
-    } else {
-        moreSecondArticle.classList.toggle("visible");
-        readMoreSecondButton.innerText = "Pokaż więcej";
-    }
-})
-
-
-
+    readMoreSecondButton.addEventListener("click", () => {
+        if (readMoreSecondButton.innerText === "Pokaż więcej") {
+            moreSecondArticle.classList.toggle("visible");
+            readMoreSecondButton.innerText = "Pokaż mniej";
+        } else {
+            moreSecondArticle.classList.toggle("visible");
+            readMoreSecondButton.innerText = "Pokaż więcej";
+        }
+    })
+}
